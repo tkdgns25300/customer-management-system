418ee28405eeec8c70ff618e45db8b0c872a84a0
unknown <tkdgns25300@gmail.com>
2022-02-02 22:16:20 +0900

    setup Customer component

---
+++ client/src/components/Customer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        <div>
+            <h1>홍길동</h1>
+            <p>000729</p>
+            <p>남자</p>
+            <p>대학생</p>
+        </div>
+    }
+}
+
+export default Customer;
