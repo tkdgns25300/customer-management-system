286ff438de671eada7eee703589039bbfb6c294c
unknown <tkdgns25300@gmail.com>
2022-02-04 23:27:45 +0900

    Structuring the Customer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Customer from './components/Customer';
 
 const customer = {
+  id: 1,
+  image: 'https://placeimg.com/64/64/any',
   name: '이상훈',
   birthday: '000729',
   gender: 'male',
@@ -13,6 +15,8 @@ const customer = {
 function App() {
   return (
     <Customer
+      id={customer.id}
+      image={customer.image}
       name={customer.name}
       birthday={customer.birthday}
       gender={customer.gender}
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
-const Customer = ({ name, birthday, gender, job }) => {
+const Customer = ({ id, image, name, birthday, gender, job }) => {
     return (
         <div>
-            <h1>{name}</h1>
+            <CustomerProfile id={id} image={image} name={name} />
+            <CustomerInfo birthday={birthday} gender={gender} job={job} />
+        </div>
+    )
+}
+
+const CustomerProfile = ({id, image, name}) => {
+    return (
+        <div>
+            <img src={image} alt="profile" />
+            <h2>{name}({id})</h2>
+        </div>
+    )
+}
+
+const CustomerInfo = ({ birthday, gender, job}) => {
+    return (
+        <div>
             <p>{birthday}</p>
             <p>{gender}</p>
             <p>{job}</p>
         </div>
     )
 }
+
 export default Customer;
